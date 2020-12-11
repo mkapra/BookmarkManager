a7ad96aed73d81b7361f9cfb66c20373125a5f39
Maximilian Kapra <maximilian@kapra.de>
2020-12-11 23:22:12 +0100

    Remove test secret key

--- bookmarkmgr/settings.py
+++ bookmarkmgr/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'q+j7+@_)fozlyg0wg!8k3%#1tm((**ky+ki63kpus^bfzh5k#r'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
